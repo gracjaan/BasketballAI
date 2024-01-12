21e7ef3e7251462ef450a72b406b276c211d4282
Thomas B <t.ferb1@gmail.com>
2024-01-12 12:11:33 +0100

    fix: fixed tailwind not reloading on js change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./templates/**/*.html",
-    "./static/src/**/*.js"
-  ],
-  theme: {
-    colors: {
-      primary: '#d9d9d9' 
-    },
-    extend: {
-      fontFamily: {
-        'montserrat': ['Montserrat'],
-        'lato': ['Lato'],
-        'garamond': ['Garamond']
-    }
-    },
-  },
-  plugins: [],
-}
-
+	content: ["./templates/**/*.html", "./static/**/*.js"],
+	theme: {
+		colors: {
+			primary: "#d9d9d9",
+		},
+		extend: {
+			fontFamily: {
+				montserrat: ["Montserrat"],
+				lato: ["Lato"],
+				garamond: ["Garamond"],
+			},
+		},
+	},
+	plugins: [],
+};
